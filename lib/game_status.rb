54a87636c94c120713b483f98e80d7c570767272
Karl Richardson <karlrichii@hotmail.com>
2019-06-09 18:45:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,10 @@ position_1 = board[win_index_1]
 position_2 = board[win_index_2]
 position_3 = board[win_index_3]
 
-
+if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination
+    
+    
 
 
 
