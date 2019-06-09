4bae9c3378909e2b32bc97956e658085122ac21a
Karl Richardson <karlrichii@hotmail.com>
2019-06-09 18:11:08 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,8 +6,7 @@ end
 # Define your WIN_COMBINATIONS constant
 
 def WIN_COMBINATIONS
-  WIN_COMBINATIONS = [
-    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,6], [2,5,8], [2,4,6], [0,4,8] ]
+  WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,6], [2,5,8], [2,4,6], [0,4,8] ]
   end
 
 WIN_COMBINATIONS
