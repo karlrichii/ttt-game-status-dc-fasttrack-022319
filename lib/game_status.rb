8f8356e49a85ecaeaf2e6a0a5dd22c7e16826762
Karl Richardson <karlrichii@hotmail.com>
2019-06-09 18:24:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,3 +8,13 @@ end
   WIN_COMBINATIONS = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [6,4,2], [0,4,8] ]
 # won Method
 
+win_index_1 = win_combination[0]
+win_index_2 = win_combination[1]
+win_index_3 = win_combination[2]
+win_index_4 = win_combination[3]
+win_index_5 = win_combination[4]
+win_index_6 = win_combination[5]
+win_index_7 = win_combination[6]
+win_index_8 = win_combination[7]
+
+position_1 = 
