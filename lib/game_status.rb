52196e9d810110ad99e775aaf1e8cd87bad8da4b
Karl Richardson <karlrichii@hotmail.com>
2019-06-09 18:44:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,20 +15,16 @@ def won?(board)
 win_index_1 = win_combination[0]
 win_index_2 = win_combination[1]
 win_index_3 = win_combination[2]
-win_index_4 = win_combination[3]
-win_index_5 = win_combination[4]
-win_index_6 = win_combination[5]
-win_index_7 = win_combination[6]
-win_index_8 = win_combination[7]
 
-position_1 = board[0,1,2]
-position_2 = board[3,4,5]
-position_3 = board[6,7,8]
-position_4 = board[0,3,6]
-position_5 = board[1,4,7]
-position_6 = board[2,5,8]
-position_7 = board[6,4,2]
-position_8 = board[0,4,8]
+
+position_1 = board[win_index_1]
+position_2 = board[win_index_2]
+position_3 = board[win_index_3]
+
+
+
+
+
 
 end
 
